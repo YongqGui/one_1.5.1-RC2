9eb5f6b9ebceced566aecfae4c2dfd7ca5898f74
YongqGui <2363179294@qq.com>
2016-11-10 20:34:11 +0800

    实现对界面进行分割，后面继续添加组件

--- gui_test/Main_Window1.java
+++ gui_test/Main_Window1.java
@@ -1,9 +1,13 @@
 package gui_test;
 
 import java.awt.*;
+
 import javax.swing.*;
 import javax.swing.event.*;
+
 import java.awt.event.*;
+
+import gui_test.Menu;
 
 public class Main_Window1 extends JFrame {
 	/** Default width for the GUI window */
@@ -12,37 +16,60 @@ public class Main_Window1 extends JFrame {
 	public static final int WIN_DEFAULT_HEIGHT = 700;
 	/** log panel's initial weight in the split panel */
 	private static final double SPLIT_PANE_LOG_WEIGHT = 0.2;
+	//private static final Component JSP3 = null;
 	
 	private JScrollPane playFieldScroll;
-	
+	private static JSplitPane JSP1;
+	private static JSplitPane JSP2;
+	private static JSplitPane JSP3;
 	//---------------------------设置tab页面----------------------------//
 	  private String[] flavors = { "路由模块", "运动模块", "缓存模块","链路模块" };
 	  private static JTabbedPane tabs = new JTabbedPane();
 	  //private JTextField txt = new JTextField(20);
 	  public Main_Window1() {
-		   int i = 0;
-		   for(String flavor : flavors)
-		     tabs.addTab(flavors[i],new JButton("Tabbed pane " + i++));
-		   //add(BorderLayout.SOUTH, txt);
-		   add(tabs);
+		  final JMenu[] menus = {
+					new JMenu("File"), new JMenu("Edit"),
+					new JMenu("Properties"),new JMenu("Tools"),
+					new JMenu("Windows"),new JMenu("Help"),
+			};
+		  final JMenuItem[] items = {
+					new JMenuItem("Fee"), new JMenuItem("Fi"),
+					new JMenuItem("Fo"),  new JMenuItem("Zip"),
+					new JMenuItem("Zap"), new JMenuItem("Zot"),
+					new JMenuItem("Olly"), new JMenuItem("Oxen"),
+					new JMenuItem("Free"), new JMenuItem("Zot"),
+					new JMenuItem("Olly"), new JMenuItem("Oxen"),
+					new JMenuItem("Olly"), new JMenuItem("Oxen"),
+					new JMenuItem("Free"), new JMenuItem("Zot"),
+					new JMenuItem("Olly"), new JMenuItem("Oxen"),
+			};
+			for (int i=0;i<items.length; i++){
+				menus[i % 6].add(items[i]);
+			};
+			JMenuBar mb = new JMenuBar();
+			for (JMenu jm:menus){
+				mb.add(jm);
+			};
+	  JLabel label1=new JLabel("Label 1",JLabel.CENTER); 
+	  JLabel label2=new JLabel("Label 2",JLabel.CENTER);    
+	  JLabel label3=new JLabel("Label 3",JLabel.CENTER);      
+	  JLabel label4=new JLabel("Label 4",JLabel.CENTER);
+
+
+      JSP1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT,false,label1,label2);
+	  JSP1.setResizeWeight(0.8);								//	设置splitPane1的分隔线位置，0.1是相对于splitPane1的大小而定。
+      JSP2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,false,JSP1,label3);
+	  JSP2.setResizeWeight(0.95);
+      JSP3 = new JSplitPane(JSplitPane.VERTICAL_SPLIT,false,mb,JSP2);	
+	  JSP3.setResizeWeight(0.01);
+
 	  }
-	  
 
-      static JLabel label1=new JLabel("Label 1",JLabel.CENTER); 
-      static JLabel label2=new JLabel("Label 2",JLabel.CENTER);
-      static JLabel label3=new JLabel("Label 3",JLabel.CENTER);
-      static JLabel label4=new JLabel("Label 4",JLabel.CENTER);
 
-      static JSplitPane JSP1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT,false,label1,label2);
-      static JSplitPane JSP2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,false,JSP1,label3);
-      static JSplitPane JSP3 = new JSplitPane(JSplitPane.VERTICAL_SPLIT,false,label4,JSP2);
 
       
 	  public static void main(String[] args) throws Exception {
 		    JFrame frame = new Main_Window1();
-		    JSP1.setResizeWeight(0.8);								//	设置splitPane1的分隔线位置，0.1是相对于splitPane1的大小而定。
-		    JSP2.setResizeWeight(0.95);
-		    JSP3.setResizeWeight(0.05);
 		    frame.add(JSP3);
 		    frame.setTitle("The ONE Simulator");
 		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
